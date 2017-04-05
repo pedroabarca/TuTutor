36f1c5a5e7c62f8583fe0c802ded5561f00d5e2a
Juan Daniel <jvargas@go-labs.net>
2017-04-04 21:51:40 -0600

    Changes

--- src/app/register/steppers/photo/photo-stepper.component.ts
+++ src/app/register/steppers/photo/photo-stepper.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, ViewChild, ElementRef, Renderer } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms';
+import { AngularFire } from 'angularfire2';
 import * as firebase from 'firebase';
 import { MdlModule } from 'angular2-mdl';
+import { StepperComponent } from '../../../shared/stepper.component';
 import { UserService } from '../../../services/user.service';
 
 @Component({
@@ -11,34 +13,60 @@ import { UserService } from '../../../services/user.service';
   styleUrls: ['./photo-stepper.component.css']
 })
 
-export class PhotoStepperComponent implements OnInit {
+export class PhotoStepperComponent extends StepperComponent implements OnInit {
 
   @ViewChild('photo') photoElement:ElementRef; 
   photo:string;
   preview:string;
 
-  constructor(private router:Router, private userService:UserService, private formBuilder:FormBuilder) {
-    this.preview = './assets/img/user_default.png';
-  }
-
   ngOnInit() {
-    this.buildForm();
+    this.subscribe();
     this.getUserProperties();
   }
-  buildForm():void {
+  subscribe():void {
+    this.authSubscription = this.angularFire.auth.subscribe(
+      auth => {
+        if(auth !== null) {
+          /*this.preview = this.setImage(auth);
+          this.photo = this.preview;*/
+        }
+      }
+    );
   }
   getUserProperties():void {
+    this.loadImage(this.userService.getPhotoUrl());
   }
   setUserProperties():void {
     this.userService.setPhotoUrl(this.photo);
   }
+  setImage(auth:any):any {
+    if(auth.provider === 2)
+      return auth.auth.photoURL;
+    else if(auth.provider === 3)
+      return auth.auth.photoURL;
+    else
+      return './assets/img/user_default.png';
+  }
+  loadImage(file:any):void {
+    console.log(file);
+    console.log((typeof file === 'object'));
+    if(typeof file === 'object') {
+      let reader:FileReader = new FileReader();
+      reader.onloadend = () => {
+        this.preview = reader.result;
+      }
+      reader.readAsDataURL(file);
+    }
+  }
   onChange(event):void {
     this.photo = event.srcElement.files[0];
+    this.userService.setPhotoUrl(this.photo);
     let reader:FileReader = new FileReader();
     reader.onloadend = () => {
       this.preview = reader.result;
     }
     reader.readAsDataURL(event.srcElement.files[0]);
+    console.log(this.photo);
     /*let storageRef = firebase.storage().ref('images');
     storageRef.put(this.photo)
     .then(function(snapshot) {
@@ -49,13 +77,10 @@ export class PhotoStepperComponent implements OnInit {
       console.log(error);
     });*/
   }
-  selectFile():void {
-    this.photoElement.nativeElement.click();
-  }
   previousStepper():void {
-    this.router.navigateByUrl('register/phone');
-  }
-  uploadPhoto():void {
+    console.log(this.userService.getPhotoUrl());
+    //this.router.navigateByUrl('register/phone');
+
   }
   nextStepper():void {
     this.setUserProperties();
